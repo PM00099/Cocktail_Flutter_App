6fdc5b08368213ca8645f68cfb6c5a3c2c4f48f6
Priyank Mithaiwala <priyankmithaiwala@gmail.com>
2020-09-10 19:39:02 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:cocktailapp/homepage.dart';
+
+
+const Color myColor = Colors.red;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Cocktail App",
+      theme: ThemeData(
+        primarySwatch: myColor,
+        fontFamily: "Pacifico",
+      ),
+      home: HomePage(),
+    );
+  }
+}
